8f3d2badc7099c6a4ba5c92d122fba5b916d8850
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-04-10 17:20:23 +0200

    update

--- scripts/search/search.js
+++ scripts/search/search.js
@@ -71,20 +71,46 @@ function advancedInputFiltering(ingredients, appliances, ustensils) {
 	for(let i = 0; i < advancedInput.length; i++) {
 		advancedInput[i].addEventListener("input", () => {
 			const filteredIngredients = ingredients.filter(ingredient => {
-                return ingredient.toLowerCase().includes(advancedInput[0].value.toLowerCase())
+                return ingredient.toLowerCase().includes(advancedInput[i].value.toLowerCase())
             })
  			const filteredAppliances = appliances.filter(appliance => {
-                return appliance.toLowerCase().includes(advancedInput[1].value.toLowerCase());
+                return appliance.toLowerCase().includes(advancedInput[i].value.toLowerCase());
             })
  			const filteredUstensils = ustensils.filter(ustensil => {
-                return ustensil.toLowerCase().includes(advancedInput[2].value.toLowerCase())
+                return ustensil.toLowerCase().includes(advancedInput[i].value.toLowerCase())
             })
-			lists[0].innerHTML = ""
-			for(let j=0; j<filteredIngredients.length; j++) {
+			console.log(filteredIngredients)
+			console.log(filteredAppliances)
+			console.log(filteredUstensils)
+			lists[i].innerHTML = ""
+
+			function test(element, index) {
+				for(let j=0; j<element.length; j++) {
+					const newSpan = document.createElement("span");
+					lists[index].appendChild(newSpan);
+					newSpan.textContent = element[j]
+				}
+			}
+
+			test(filteredIngredients, 0)
+			test(filteredAppliances, 1)
+			test(filteredUstensils, 2)
+			
+/* 			for(let j=0; j<filteredIngredients.length; j++) {
 				const ingredientSpan = document.createElement("span");
 				lists[0].appendChild(ingredientSpan)
 				ingredientSpan.textContent = filteredIngredients[j]
 			} //split function
+			for(let j=0; j<filteredAppliances.length; j++) {
+				const applianceSpan = document.createElement("span");
+				lists[1].appendChild(applianceSpan)
+				applianceSpan.textContent = filteredAppliances[j]
+			}
+			for(let j=0; j<filteredAppliances.length; j++) {
+				const ustensilSpan = document.createElement("span");
+				lists[2].appendChild(ustensilSpan)
+				ustensilSpan.textContent = filteredUstensils[j]
+			} */
 			return filteredIngredients || filteredAppliances || filteredUstensils
 		})
 	}
@@ -116,6 +142,7 @@ function createTag(recipes) {
 			const spans = lists[i].getElementsByTagName("span")
 			for(let j = 0; j<spans.length; j++) {
 				spans[j].addEventListener("click", () => {
+					console.log("click")
 					console.log(spans[j].innerHTML)
 					const tag = document.createElement("span")
 					tag.classList.add("tag");
@@ -123,9 +150,8 @@ function createTag(recipes) {
 					tag.textContent = spans[j].innerHTML
 
 					const filteredRecipes = recipes.filter(recipe => {
-						console.log(recipe)
-						return recipe.ingredients.ingredient.includes(tag.innerHTML) ||
-						recipe.appliance.includes(tag.innerHTML)
+						
+						return recipe.appliance.includes(tag.innerHTML)
 					})
 					recipeSection.innerHTML = "";
 					createRecipeCard(filteredRecipes)

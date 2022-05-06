3017b364bb8c80b9f6ce64147c45d20999a40cd0
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-05-06 15:30:54 +0200

    update

--- scripts/search/search.js
+++ scripts/search/search.js
@@ -110,7 +110,7 @@ function createTag(recipes) {
 				const isFound = tags.find(elementTag => elementTag === span.innerHTML)
 				if(!isFound) {
 					const tag = document.createElement("span");
-					tag.classList.add(`tag${index}`);
+					tag.classList.add(`tag${index}`, "tag");
 					tag.setAttribute("datatype", dataType)
 					tagSection.appendChild(tag);
 					tag.innerHTML = span.innerHTML + "<i class='fa-solid fa-xmark delete-tag'></i>";
@@ -128,6 +128,46 @@ function createTag(recipes) {
 
 
 //double tableau tags + datatypes liée, création de tags pour l'un puis ajout du datatype dans le second tableau
+/* async function deleteTag() {
+	const { recipes } = await getRecipes(); //hors fonction
+	const crossTags = document.querySelectorAll(".delete-tag"); //Croix pour suppression de tags
+	
+	if(tagSection.innerHTML == "") return;
+	else {
+		crossTags.forEach(crossTag => {
+			crossTag.addEventListener("click", () => {
+				crossTag.parentElement.remove();
+				//Filtrage des tags
+				tags = tags.filter(elementTag => { return elementTag != crossTag.parentElement.innerText })
+				//si il reste des tags alors filtrage des recettes sinon filteredRecipe = recipes
+				if(tags.length === 0) {
+					if(input.value.length >= 3) filterWithInputValue(recipes)
+					else filteredRecipes = recipes
+					createRecipeCard(filteredRecipes)
+					mainInputFiltering(filteredRecipes)
+				}
+				if(tags.length >= 1) {
+					if(input.value.length >= 3) filterWithInputValue(recipes)
+					else filteredRecipes = recipes
+					tags.forEach(elementTag => {
+						filteredRecipes = filteredRecipes.filter(recipe => {
+							const checkIngredients = recipe.ingredients.filter(({ingredient}) => { 
+								console.log(ingredient)
+								ingredient.includes(elementTag)
+							}) 
+/						const checkAppliance = recipe.appliance.includes(elementTag);
+							const checkUstensils = recipe.ustensils.includes(elementTag); 
+							return  checkIngredients   ||  checkAppliance || checkUstensils 
+						})
+						console.log("recettes filtrées",filteredRecipes)
+					})
+					createRecipeCard(filteredRecipes)
+				};
+			});
+		});
+	};
+}; */
+
 async function deleteTag() {
 	const { recipes } = await getRecipes(); //hors fonction
 	const crossTags = document.querySelectorAll(".delete-tag"); //Croix pour suppression de tags
@@ -147,21 +187,50 @@ async function deleteTag() {
 					mainInputFiltering(filteredRecipes)
 				}
 				if(tags.length >= 1) {
-					if(input.value.length != 0) filterWithInputValue(recipes)
+					if(input.value.length >= 3) filterWithInputValue(recipes)
 					else filteredRecipes = recipes
-					tags.forEach(elementTag => {
-						filteredRecipes = filteredRecipes.filter(recipe => {
-/* 							const checkIngredients = recipe.ingredients.filter(({ingredient}) => { 
-								console.log(ingredient)
-								ingredient.includes(elementTag)
-							}) */
-							const checkAppliance = recipe.appliance.includes(elementTag);
-							const checkUstensils = recipe.ustensils.includes(elementTag);
-							return /* checkIngredients   || */ checkAppliance || checkUstensils
-						})
-						console.log("recettes filtrées",filteredRecipes)
+					
+					//On selection les tags présents sur le dom
+					const tagList = document.querySelectorAll(".tag");
+					
+
+					//Pour chaque tag, on récupère les recettes filtrées pour CET+
+					
+					tagList.forEach((tag, index) => {
+
+						//Idée : Push toute les recettes issues du filtrages dans un même tableau puis ne concervent que ceux n'apparaissant
+						//autant de foisq ue le nombre de tag restants => si 3 tags, si une recette ressort 3 fois, alors => filteredRecipes
+						if(tag.classList.contains("tag0")) {
+							//filteredRecipes = [];
+							let array = []
+							let pushedArray = []
+							filteredRecipes.push(filteredRecipes.filter(recipe => {
+								recipe.ingredients.filter(({ingredient}) => {
+									if(ingredient.includes(tag.innerText)) return array.push(recipe);
+								});
+							}));
+							console.log(array)
+							
+							array.forEach(element => {
+								
+								pushedArray.push(element)
+							})
+							/* array = array.slice(0, array.length-1) *///filteredRecipes.slice(0, filteredRecipes.length-1)
+							console.log("array", array)
+							//filteredRecipes = array
+							console.log("recetets" ,pushedArray)
+						}
+						if(tag.classList.contains("tag1")) filteredRecipes = filteredRecipes.filter(recipe => recipe.appliance.includes(tag.innerText))
+						if(tag.classList.contains("tag2")) filteredRecipes = filteredRecipes.filter(recipe => recipe.ustensils.includes(tag.innerText))
+
+						console.log("tag", tag.innerText, "recette filtrée", filteredRecipes)
 					})
+					filteredRecipes = filteredRecipes.slice(0, filteredRecipes.length-1)
+					console.log(filteredRecipes)
 					createRecipeCard(filteredRecipes)
+					
+
+					//Affichage
 				};
 			});
 		});
